ffba500f1fc2c896ec16931e42e7b8e4668c8959
bernardh75 <33285622+bernardh75@users.noreply.github.com>
2020-05-05 16:26:12 +0200

    Update io.js

--- io/io.js
+++ io/io.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
+
 module.exports.decodeHexFileContent = (filePath) =>  {
     return new Promise((resolve, reject) => {
-        // To be implemented!
+        let encoded = fs.readFileSync(filePath, 'utf8');
+        let res = Buffer.from(encoded, 'hex').toString('utf8');
+        
+        resolve(res);
     });
 }
