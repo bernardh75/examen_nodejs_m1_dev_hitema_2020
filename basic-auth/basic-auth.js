16338e29c7b7030b1736e9c036d47a14ba717707
bernardh75 <33285622+bernardh75@users.noreply.github.com>
2020-05-05 16:21:42 +0200

    Update basic-auth.js

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -1,9 +1,16 @@
 const crypto = require('crypto');
 
 function sha1Encode(data) {
-    // To be implemented!
+    return crypto.createHash('sha1').update(data).digest('hex');
 }
 
 module.exports.digestAuth = (request, response, next) => {
-    // To be implemented!
+    const encoded = authorization.replace('Basic ', '');
+    const decoded = Buffer.from(encoded, 'base64').toString('utf8');
+    
+    const authentication = decoded.split(':');
+    const authorization = request.headers.authorization;
+    
+    const isValid = authentication[0] === 'node' && authentication[1] === sha1Encode('password');
+    isValid ? next() : response.sendStatus(401);
 }
